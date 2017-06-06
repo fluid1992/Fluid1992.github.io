d51e12d61a93300d3068c2dd1e24b751d2ef8a21
fluid1992 <pmc692442135@qq.com>
2017-06-06 18:25:44 +0800

    checkAll
    pmc

--- myQuery/myPackage/js/myQuery.js
+++ myQuery/myPackage/js/myQuery.js
@@ -1,3 +1,11 @@
 function $getId(id) {
 	return document.getElementById(id);
 }
+//获取元素属性值getStyle
+function getStyle(obj, strr) {
+	if(obj.currentStyle) {
+		return obj.currentStyle[strr];
+	} else {
+		return getComputedStyle(obj)[strr];
+	}
+}
