6286740d2215a59cb90c0977a11d32f9ed2a2b8f
fluid1992 <pmc692442135@qq.com>
2017-06-07 18:28:22 +0800

    up or down
    pmc

---
+++ wo/js/MyQuery.js
@@ -0,0 +1,11 @@
+function $(id) {
+	return document.getElementById(id);
+}
+//获取元素属性值getStyle
+function getStyle(obj, strr) {
+	if(obj.currentStyle) {
+		return obj.currentStyle[strr];
+	} else {
+		return getComputedStyle(obj)[strr];
+	}
+}
