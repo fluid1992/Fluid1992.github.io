f8de3c68d5989e6abc5a9b4cfdf5ff779b886816
fluid1992 <pmc692442135@qq.com>
2017-06-09 08:57:58 +0800

    jun
    pmc

--- wo/js/MyQuery.js
+++ wo/js/MyQuery.js
@@ -2,10 +2,27 @@ function $(id) {
 	return document.getElementById(id);
 }
 //获取元素属性值getStyle
-function getStyle(obj, strr) {
+function getStyle(obj, strr){
 	if(obj.currentStyle) {
 		return obj.currentStyle[strr];
 	} else {
 		return getComputedStyle(obj)[strr];
 	}
+}
+//元素的移动
+function Go(obj, moveNum, target, strr, funEnd) {
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function() {
+		var maLeft = parseInt(getStyle(obj, strr)) + moveNum;
+		if(maLeft < target && moveNum < 0 || maLeft > target && moveNum > 0) {
+			maLeft = target;
+		}
+		obj.style[strr] = maLeft + 'px';
+		if(maLeft == target) {
+			clearInterval(obj.timer);
+			if(funEnd) {
+				funEnd();
+			}
+		}
+	}, 50)
 }
