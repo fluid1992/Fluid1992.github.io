4fea4667df749aea9e85acdb671c6b869ff160d5
fluid1992 <pmc692442135@qq.com>
2017-07-17 18:30:05 +0800

    ya
    pmc

--- safeManager/js/safeManager.js
+++ safeManager/js/safeManager.js
@@ -1,42 +1,25 @@
-var len = $('.right-nav li').lenght;
+var len = $('.right-nav li').length;
 $('.right-nav li').each(function(i,elem){
 	$(elem).css('background-position',39*i+'px 0px')
 })
 var scrollControl = 0;
 	$('.right-nav li').eq(scrollControl).css('background-position',39*scrollControl+'px 39px');
-//$(document).mousewheel(function(){
-//	scrollControl ++;
-//	if(scrollControl > len - 1){
-//		scrollControl = 0;
-//	}
-//	console.log(1)
-//	$('.right-nav li').each(function(i,elem){
-//		$(elem).css('background-position',39*i+'px 0px')
-//	})
-//	$('.right-nav li').eq(scrollControl).css('background-position',39*scrollControl+'px 39px');
-//})
-var addMouseWheelHandler = function() {
-  document.addEventListener("mousewheel", MouseWheelHandler, false); 
+
+//var addMouseWheelHandler = function() {
+  document.addEventListener("mousewheel", MouseWheelHandler, false); //不兼容火狐
   //IE9, Chrome, Safari, Oper
-}
-function MouseWheelHandler(){
-	scrollControl ++;
+//}
+
+function MouseWheelHandler(delta){
+	delta.deltaY > 0 ? scrollControl ++ : scrollControl -- ;
 	if(scrollControl > len - 1){
 		scrollControl = 0;
+	}else if(scrollControl < 0){
+		scrollControl = len - 1;
 	}
-	console.log(1)
 	$('.right-nav li').each(function(i,elem){
 		$(elem).css('background-position',39*i+'px 0px')
 	})
 	$('.right-nav li').eq(scrollControl).css('background-position',39*scrollControl+'px 39px');
 }
-addMouseWheelHandler();
-//$(document).ready(function(){
-//$(window).scroll(function(){
-//	alert(1);
-//  if($(window).scrollTop()>100){
-////     $("div").animate({left:'250px'});  
-//
-//    });
-// });
-//});
+//上面是右边的那个滑动导航
