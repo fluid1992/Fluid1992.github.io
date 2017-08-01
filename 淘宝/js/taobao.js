3b58ba317847219d46f8ce940cf5443a10b5f859
fluid1992 <pmc692442135@qq.com>
2017-08-01 17:48:26 +0800

    save
    pmc

---
+++ 淘宝/js/taobao.js
@@ -0,0 +1,6 @@
+$('.mytb-nav').mouseover(function(){
+	$('.mytb-nav .pulldown').css('display','block');
+})
+$('.mytb-nav').mouseout(function(){
+	$('.mytb-nav .pulldown').css('display','none');
+})
