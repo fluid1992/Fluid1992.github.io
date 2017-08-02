85824fac21ab852dc5ea3999fe2baca13cff0260
fluid1992 <pmc692442135@qq.com>
2017-08-02 18:05:23 +0800

    nnn
    pmc

--- 淘宝/js/taobao.js
+++ 淘宝/js/taobao.js
@@ -4,3 +4,35 @@ $('.mytb-nav').mouseover(function(){
 $('.mytb-nav').mouseout(function(){
 	$('.mytb-nav .pulldown').css('display','none');
 })
+
+$('.area').mouseover(function(){
+	$('.area .pulldown').css('display','block');
+})
+
+$('.area').mouseout(function(){
+	$('.area .pulldown').css('display','none');
+})
+$('.my-area li').click(function(){
+	$('.area span').html($(this).html());
+})
+
+$('.mycollect').mouseover(function(){
+	$('.mycollect .pulldown').css('display','block');
+})
+$('.mycollect').mouseout(function(){
+	$('.mycollect .pulldown').css('display','none');
+})
+
+$('.buy-person').mouseover(function(){
+	$('.buy-person .pulldown').css('display','block');
+})
+$('.buy-person').mouseout(function(){
+	$('.buy-person .pulldown').css('display','none');
+})
+
+$('.contact-service').mouseover(function(){
+	$('.contact-service .pulldown').css('display','block');
+})
+$('.contact-service').mouseout(function(){
+	$('.contact-service .pulldown').css('display','none');
+})
