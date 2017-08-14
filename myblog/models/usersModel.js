205de8e9c67412ca77b8c82049c6d388bf763757
fluid1992 <pmc692442135@qq.com>
2017-08-14 10:57:35 +0800

    11
    111

--- myblog/models/usersModel.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-var userSchema = require('../schemas/userName');
-var userModel = mongoose.model('userModel',userSchema);
-module.exports=userModel;
--- myblog/schemas/userName.js
+++
@@ -1,7 +0,0 @@
-var mongoose =require('mongoose');
-var schma = mongoose.Schema;
-var userSchema = new schma({
-    userEmail:String,
-    password:String
-})
-module.exports=userSchema;
